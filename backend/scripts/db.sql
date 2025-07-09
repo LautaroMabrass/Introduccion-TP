8635ca740b1347778947a6859e7ce3faac3b07a4
Lautaro Mabras <mabraslautaroezequiel@hotmail.com>
2025-07-09 20:52:02 -0300

    Configuracion incial de la api y creacion del script para la db

--- backend/scripts/db.sql
+++ backend/scripts/db.sql
@@ -9,36 +9,32 @@ CREATE TABLE suenios_lucidos (
     id SERIAL PRIMARY KEY,
     firma VARCHAR(100),
     contenido TEXT,
-    fecha DATE
-);
-
-CREATE TABLE comentarios_lucidos (
-    id SERIAL PRIMARY KEY,
-    contenido TEXT,
-    suenio_lucido_id INTEGER REFERENCES suenios_lucidos(id)
-);
-
-CREATE TABLE niveles_de_control (
-    id SERIAL PRIMARY KEY,
-    nivel TEXT,
-    suenio_lucido_id INTEGER REFERENCES suenios_lucidos(id)
+    fecha DATE,
+    nivel_de_lucidez INTEGER,
 );
 
 CREATE TABLE suenios_personales (
     id SERIAL PRIMARY KEY,
     firma VARCHAR(100),
     contenido TEXT,
-    fecha DATE
+    fecha DATE,
+    emociones TEXT
+);
+
+CREATE TABLE comentarios_lucidos (
+    id SERIAL PRIMARY KEY,
+    contenido TEXT,
+    suenio_lucido_id INTEGER REFERENCES suenios_lucidos(id) ON DELETE CASCADE
 );
 
 CREATE TABLE comentarios_personales (
     id SERIAL PRIMARY KEY,
     contenido TEXT,
-    suenios_personales_id INTEGER REFERENCES suenios_personales(id)
+    suenios_personales_id INTEGER REFERENCES suenios_personales(id) ON DELETE CASCADE
 );
 
 CREATE TABLE emociones (
     id SERIAL PRIMARY KEY,
     emocion TEXT,
-    suenios_personales_id INTEGER REFERENCES suenios_personales(id)
+    suenios_personales_id INTEGER REFERENCES suenios_personales(id) ON DELETE CASCADE
 );
