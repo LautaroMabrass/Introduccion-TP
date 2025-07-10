17b24d4e8f5d659724712902cbdc4647fad748a4
Lautaro Mabras <mabraslautaroezequiel@hotmail.com>
2025-07-10 17:34:50 -0300

    Acceso a la db para sueños lucidos y comentarios de sueños lucidos

--- backend/src/acceso-db.js
+++ backend/src/acceso-db.js
@@ -30,9 +30,27 @@ const createSuenio = async (firma, contenido, fecha, emociones) => {
   return result.rows[0];
 }
 
+// Funciones de acceso a la base de datos para los sueños lucidos
+const getAllSueniosLucidos = async () => {
+  const result = await dbClient.query('SELECT * FROM suenios_lucidos');
+  if (result.rowCount === 0) {
+    return [];
+  }
+  return result.rows;
+}
+
+// Crear un nuevo sueño lucido
+const createSuenioLucido = async (firma, contenido, fecha, emociones) => {
+  const result = await dbClient.query('INSERT INTO suenios_lucidos (firma, contenido, fecha, emociones) VALUES ($1, $2, $3, $4) RETURNING *', [firma, contenido, fecha, emociones]);
+  if (result.rowCount === 0) {
+    return undefined;
+  }
+  return result.rows[0];
+}
+
 // Funciones de acceso a la base de datos para los comentarios
 
-// Obtener comentarios relacionados a un sueño específico
+// Obtener comentarios relacionados a un sueño específico (Sueños personales UNICAMENTE)
 const getComentariosRelacionadosASuenios = async (id_suenio) => {
   const result = await dbClient.query('SELECT * FROM comentarios_personales WHERE suenios_personales_id = $1', [id_suenio]);
   if (result.rowCount === 0) {
@@ -41,7 +59,7 @@ const getComentariosRelacionadosASuenios = async (id_suenio) => {
   return result.rows;
 }
 
-// Crear un nuevo comentario personal
+// Crear un nuevo comentario personal (Sueños personales UNICAMENTE)
 const createComentarioPersonal = async (contenido, suenios_personales_id) => {
   const result = await dbClient.query('INSERT INTO comentarios_personales (contenido, suenios_personales_id) VALUES ($1, $2) RETURNING *', [contenido, suenios_personales_id]);
   if (result.rowCount === 0) {
@@ -50,9 +68,36 @@ const createComentarioPersonal = async (contenido, suenios_personales_id) => {
   return result.rows[0];
 }
 
-// Eliminar un comentario personal
+// Eliminar un comentario personal (Sueños personales UNICAMENTE)
 const deleteOneComentarioPersonal = async (id_comentario) => {
   const result = await dbClient.query('DELETE FROM comentarios_personales WHERE id = $1 RETURNING *', [id_comentario]);
+  if (result.rowCount === 0) {
+    return undefined;
+  }
+  return result.rows[0];
+}
+
+// Obtener un comentario a un sueño especifico (Sueños lucidos UNICAMENTE)
+const getComentariosRelacionadosASueniosLucidos = async (id_suenio) => {
+  const result = await dbClient.query('SELECT * FROM comentarios_lucidos WHERE suenios_lucidos_id = $1', [id_suenio]);
+  if (result.rowCount === 0) {
+    return [];
+  }
+  return result.rows;
+}
+
+// Crear un nuevo comentario a un sueño lucido (Sueños lucidos UNICAMENTE)
+const createComentarioSuenioLucido = async (contenido, suenios_lucidos_id) => {
+  const result = await dbClient.query('INSERT INTO comentarios_lucidos (contenido, suenios_lucidos_id) VALUES ($1, $2) RETURNING *', [contenido, suenios_lucidos_id]);
+  if (result.rowCount === 0) {
+    return undefined;
+  }
+  return result.rows[0];
+}
+
+// Eliminar un comentario a un sueño lucido (Sueños lucidos UNICAMENTE)
+const deleteOneComentarioSuenioLucido = async (id_comentario) => {
+  const result = await dbClient.query('DELETE FROM comentarios_lucidos WHERE id = $1 RETURNING *', [id_comentario]);
   if (result.rowCount === 0) {
     return undefined;
   }
@@ -64,5 +109,10 @@ module.exports = {
   getAllSuenios,
   getComentariosRelacionadosASuenios,
   createComentarioPersonal,
-  deleteOneComentarioPersonal
+  deleteOneComentarioPersonal,
+  getAllSueniosLucidos,
+  createSuenioLucido,
+  getComentariosRelacionadosASueniosLucidos,
+  createComentarioSuenioLucido,
+  deleteOneComentarioSuenioLucido
 }
