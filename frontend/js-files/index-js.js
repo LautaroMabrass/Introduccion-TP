683af701417d0659b0950fd0256ca74ac5760fd7
Lautaro Mabras <mabraslautaroezequiel@hotmail.com>
2025-07-09 21:40:08 -0300

    Finalizando la creacion de los sueños personales

--- frontend/js-files/index-js.js
+++ frontend/js-files/index-js.js
@@ -6,10 +6,13 @@ const crearSuenio = () => {
     const firma = document.getElementById('firma_del_autor').value.trim();
     const emociones = document.getElementById('emociones_del_suenio').value.trim();
     const fecha = document.getElementById('fecha_del_suenio').value;
+
     const mensajeError = document.getElementById('msjerror');
+    const mensajeExito = document.getElementById('msjexito');
 
     if (!tipo || !contenido || !firma || !emociones || !fecha) {
         mensajeError.style.display = 'block';
+        mensajeExito.style.display = 'none';
         return;
     }
 
@@ -28,20 +31,30 @@ const crearSuenio = () => {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(nuevoSuenio)
         })
-            .then(response => {
-                if (response.ok) {
-                    console.log('TODO OK');
-                    return response.json();
-                } else {
-                    console.error('Error al crear el sueño:', response.statusText);
-                }
-            })
-            .catch(error => {
-                console.error('Error en la solicitud:', error);
-            });
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                console.log('Error al crear el sueño');
+            }
+        })
+        .then(data => {
+            console.log('Sueño creado:', data);
+            mensajeExito.style.display = 'block';
+
+            // limpiar los campos del formulario
+            document.getElementById('suenio').value = '';
+            document.getElementById('firma_del_autor').value = '';
+            document.getElementById('emociones_del_suenio').value = '';
+            document.getElementById('fecha_del_suenio').value = '';
+            const radios = document.querySelectorAll('input[name="tipo_suenio"]');
+            radios.forEach(radio_input => radio_input.checked = false);
+        })
+        .catch(error => {
+            console.error('Error en la solicitud:', error);
+        });
     }
     else if (tipo === 'lucido') {
-        console.log('Implementar creación de sueño lucido');
+        console.log('Implementar la creacion del sueño lucido');
     }
-
 };
