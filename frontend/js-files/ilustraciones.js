419379d73b2db108d1dc53db67a652884931a581
Lautaro Mabras <mabraslautaroezequiel@hotmail.com>
2025-07-24 15:10:05 -0300

    "Mejora de estilo en ilustraciones y agregado de fecha de publicacion

--- frontend/js-files/ilustraciones.js
+++ frontend/js-files/ilustraciones.js
@@ -2,57 +2,76 @@ const imagenesApi = 'http://localhost:3000/api/imagenes';
 const usuario = localStorage.getItem('usuarioLogueado');
 const divPrincipal = document.getElementById("contenedor-principal");
 
+function renderElemento(element) {
+    const divContenedor = document.createElement('div');
+    divContenedor.className = 'contenedor-ilustraciones';
+
+    const divContenedorLista = document.createElement('div');
+    divContenedorLista.className = 'contenedor-ilustraciones-lista';
+
+    const divContenedorObjetos = document.createElement('div');
+    divContenedorObjetos.className = 'contenedor-ilustraciones-lista-datos';
+
+    divContenedor.appendChild(divContenedorLista);
+    divContenedorLista.appendChild(divContenedorObjetos);
+
+    const titulo = document.createElement('h3');
+    titulo.textContent = element.titulo;
+
+    const descripcion = document.createElement('p');
+    descripcion.textContent = element.descripcion;
+
+    const autor = document.createElement('p');
+    autor.className = 'contenedor-ilustraciones-lista-datos-autor';
+    autor.textContent = `Autor: ${element.nombre}`;
+
+    const fecha = new Date(element.fecha);
+    const fecha_elemento = document.createElement("p");
+    fecha_elemento.className = 'contenedor-ilustraciones-lista-datos-fecha';
+    fecha_elemento.textContent = `Publicado el: ${fecha.getDate()}/${fecha.getMonth() + 1}/${fecha.getFullYear()}`;
+
+    const imagen = document.createElement('img');
+    imagen.src = `http://localhost:3000/uploads/${element.url}`;
+    imagen.alt = element.titulo;
+
+    if (element.nombre === usuario) {
+        const eliminarIlustracion = document.createElement('button');
+        eliminarIlustracion.textContent = "eliminar ilustración";
+        eliminarIlustracion.onclick = () => EliminarIlustracion(divContenedorLista, element.id);
+        divContenedorObjetos.appendChild(eliminarIlustracion);
+    }
+
+    divContenedorObjetos.appendChild(titulo);
+    divContenedorObjetos.appendChild(descripcion);
+    divContenedorObjetos.appendChild(imagen);
+    divContenedorObjetos.appendChild(autor);
+    divContenedorObjetos.appendChild(fecha_elemento);
+
+    divPrincipal.appendChild(divContenedorLista);
+}
+
 fetch(imagenesApi).then(response => {
     if (response.ok) {
-        return response.json()
+        return response.json();
     } else {
-        console.log('Error al obtenes las imagenes')
+        console.log('Error al obtener las imagenes');
     }
 }).then(data => {
     if (!data) {
-        document.getElementById('div-sin-suenios').style.display = 'block'
+        document.getElementById('div-sin-suenios').style.display = 'block';
         return;
     }
     data.forEach(element => {
-        const divContenedor = document.createElement('div')
-        divContenedor.className = 'contenedor-ilustraciones'
-
-        const divContenedorLista = document.createElement('div')
-        divContenedorLista.className = 'contenedor-ilustraciones-lista'
-
-        const divContenedorObjetos = document.createElement('div')
-        divContenedorObjetos.className = 'contenedor-ilustraciones-lista-datos'
-
-        divContenedor.appendChild(divContenedorLista);
-        divContenedorLista.appendChild(divContenedorObjetos);
-
-        const titulo = document.createElement('h3')
-        titulo.textContent = element.titulo
-
-        const descripcion = document.createElement('p')
-        descripcion.textContent = element.descripcion
-
-        const autor = document.createElement('p')
-        autor.textContent = `Autor: ${element.nombre}`
-
-
-        const imagen = document.createElement('img')
-        imagen.src = `http://localhost:3000/uploads/${element.url}`;
-        imagen.alt = element.titulo;
-
         if (element.nombre === usuario) {
-            const eliminarIlustracion = document.createElement('button')
-            eliminarIlustracion.textContent = "eliminar ilustracion";
-            eliminarIlustracion.onclick = () => EliminarIlustracion(divContenedorLista, element.id);
-            divContenedorObjetos.appendChild(eliminarIlustracion)
+            renderElemento(element);
         }
-        divContenedorObjetos.appendChild(titulo);
-        divContenedorObjetos.appendChild(descripcion);
-        divContenedorObjetos.appendChild(imagen)
-        divContenedorObjetos.appendChild(autor);
-        divPrincipal.appendChild(divContenedorLista);
     });
-})
+    data.forEach(element => {
+        if (element.nombre !== usuario) {
+            renderElemento(element);
+        }
+    });
+});
 
 const EliminarIlustracion = (divContenedorLista, imagen_id) => {
     fetch(imagenesApi + `/${imagen_id}`, {
